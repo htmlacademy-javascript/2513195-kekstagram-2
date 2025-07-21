0e29c7524ee91c5efc611e7d392a5cd1b1836238
Alena Romashenko <rome.alena@gmail.com>
2025-07-21 13:19:48 +0300

    Решает 2 обязательные задачи

---
+++ js/functions.js
@@ -0,0 +1,36 @@
+// Функция для проверки длины строки
+// const string = 'привет';
+// const maxLength = 7;
+
+// function checkStringLength () {
+//   if (string.length <= maxLength) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// checkStringLength(string, maxLength);
+
+//или
+function checkStringLength (string, maxLength) {
+  return string.length <= maxLength;
+}
+
+checkStringLength('привет', 5); // выдает ошибку, если равно
+
+
+// Функция для проверки, является ли строка палиндромом
+function checkIfPalindrome (string) {
+  let upperCleanedString = string.replaceAll(' ', '').toUpperCase();
+  let reversedString = '';
+  for (let i = upperCleanedString.length - 1; i >= 0; i--) {
+    reversedString += upperCleanedString[i];
+  }
+  return upperCleanedString === reversedString;
+}
+
+checkIfPalindrome('Лёша на полке клопа нашёл ');
+
+//Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+//Если в строке нет ни одной цифры, функция должна вернуть NaN:
+
